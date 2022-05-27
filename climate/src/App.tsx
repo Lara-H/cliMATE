695907d153be74739fe9ed54a51c0c4a5a6c8e13
Lara-H <lara.heibel@yahoo.de>
2022-05-27 13:11:24 +0200

    Add Navbar + Import Bootstrap JS

--- climate/src/App.tsx
+++ climate/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Layout from './components/Layout/Layout';
 import './App.css';
 
 function App() {
   return (
     <Layout>
-
     </Layout>
   );
 }
+
 
 export default App;
