7afe34c8f8e066abf4f2adce71ab74d0b33a6a48
Lara-H <lara.heibel@yahoo.de>
2022-06-19 19:10:35 +0200

    ScrollToTop Button Mobile fixed after Scroll

--- climate/src/components/Footer/Footer.tsx
+++ climate/src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './Footer.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -7,7 +7,17 @@ import {
 
 interface FooterProps {}
 
-const Footer: FC<FooterProps> = () => (
+const Footer: FC<FooterProps> = () => {
+
+  // Scroll-to-top Button Mobile nach Scrollen fixed einblenden
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setScroll(window.scrollY > 100);
+    });
+  }, []);
+
+return(
   <footer className={[styles.Footer, "text-light", "bg-dark", "py-4"].join(' ')} data-testid="Footer">
     <div className="container">
       <div className="row align-items-baseline">
@@ -16,11 +26,12 @@ const Footer: FC<FooterProps> = () => (
           <a href="/datenschutz">Datenschutz</a>
         </div>
         <div className="col text-end">
-          <a href="#" type="button" className="btn btn-primary text-light"><FontAwesomeIcon icon={faAngleUp} /></a>
+          <a href="#" type="button" className={scroll ? `btn btn-primary text-light ${styles["cm-btn"]}` : `btn btn-primary text-light ${styles["cm-btn"]} d-none d-sm-block`}><FontAwesomeIcon icon={faAngleUp} /></a>
         </div>
       </div>
     </div>
   </footer>
 );
+};
 
 export default Footer;
